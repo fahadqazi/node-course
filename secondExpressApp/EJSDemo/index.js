5bfc1653dd3d74db709bb592160515ae38cb52d5
fahadqazi <fahadqazi@gmail.com>
2016-01-25 23:51:13 +0000

    started using ejs and made a second app

---
+++ secondExpressApp/EJSDemo/index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+
+app.get("/home", function(req, res) {
+    res.send("<h1>Hey you are in the home directory!</h1><br><ul><li>one</li><li>Two</li></ul>");
+});
+
+
+app.get("/embedded", function(req,res){
+  res.render('embedded.ejs'); 
+});
+
+
+app.get("/welcome/:firstname/:lastname", function(req,res){
+    var fname = req.params.firstname;
+    var lname = req.params.lastname;
+    
+    res.render("login.ejs", {firstname:fname, lastname:lname}); 
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server has started!");
+});
