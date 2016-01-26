4664dfae7e4f10f5bba352b754d5e880bcc2b460
fahadqazi <fahadqazi@gmail.com>
2016-01-26 06:09:36 +0000

    new practice projects

--- secondExpressApp/EJSDemo/index.js
+++ secondExpressApp/EJSDemo/index.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var app = express();
 
-app.get("/home", function(req, res) {
-    res.send("<h1>Hey you are in the home directory!</h1><br><ul><li>one</li><li>Two</li></ul>");
-});
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// app.get("/home", function(req, res) {
+//     res.send("<h1>Hey you are in the home directory!</h1><br><ul><li>one</li><li>Two</li></ul>");
+// });
 
 
 app.get("/embedded", function(req,res){
-  res.render('embedded.ejs'); 
+  res.render('embedded'); 
 });
 
 
@@ -15,7 +18,17 @@ app.get("/welcome/:firstname/:lastname", function(req,res){
     var fname = req.params.firstname;
     var lname = req.params.lastname;
     
-    res.render("login.ejs", {firstname:fname, lastname:lname}); 
+    res.render("login", {firstname:fname, lastname:lname}); 
+});
+
+app.get("/posts", function(req, res) {
+    var posts = [
+        {title: "post 1", author: "GGG"},
+        {title: "post 2", author: "Money"},
+        {title: "post 2", author: "pacman"}
+        ];
+        
+        res.render("posts", {posts: posts});
 });
 
 
